1db34e085fc1a6d6388d34fec2b5c42164865a6a
PhuLoi <luonghuuphuloi11@gmail.com>
2022-12-08 23:26:37 +0700

    create 1 file basic test

---
+++ basicGO.go
@@ -0,0 +1,17 @@
+package main
+import("fmt")
+
+//hello world code
+
+// func main() {
+// 	fmt.Println("Hello World")
+// }
+
+
+// variable
+func main() {
+	var numA int = 16;
+	numB := 18;
+	fmt.Println("Result is ", numA + numB)
+
+}
